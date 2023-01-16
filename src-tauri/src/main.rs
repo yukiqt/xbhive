f605d91899fdcb7907db798ed8cb5d353d92c95c
yuki <yuki@possum.city>
2023-01-16 22:06:40 +0100

    the

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,51 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+//use tauri_utils::config::{Config, WindowConfig};
+use tauri::Manager;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
+#[allow(unused_must_use)]
 fn main() {
+    let useragent = "XBHive Manager";
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+            main_window.with_webview(|webview| {
+                #[cfg(target_os = "linux")]
+                {
+                    use webkit2gtk::{WebViewExt, SettingsExt};
+                    let webview = webview.inner();
+                    let settings = webview.settings().unwrap();
+                    settings.set_user_agent(Some(useragent));
+                }
+
+                #[cfg(windows)]
+                unsafe {
+                    use webview2_com::Microsoft::Web::WebView2::Win32::ICoreWebView2Settings2;
+                    use windows::core::Interface;
+            
+                    let settings: ICoreWebView2Settings2 = webview
+                        .controller()
+                        .CoreWebView2()
+                        .unwrap()
+                        .Settings()
+                        .unwrap()
+                        .cast()
+                        .unwrap();
+            
+                    settings
+                        .SetUserAgent(useragent.to_string())
+                        .unwrap();
+                }
+            
+                // untested
+                #[cfg(target_os = "macos")]
+                unsafe {
+                    use objc::msg_send;
+                    use objc_foundation::{NSString, INSString};
+                    let () = msg_send![webview.inner(), setCustomUserAgent: NSString::from_str(useragent)];
+                }
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,1 @@
-const { invoke } = window.__TAURI__.tauri;
-
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
-});
+window.location.replace("https://www.thehiveforums.com/admin_e.php?xbhm/user-manager/");
