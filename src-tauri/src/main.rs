99cab6c237b9c36087f152477ed88ce9ff28b034
yuki <yuki@possum.city>
2023-01-16 23:36:03 +0100

    add title

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,13 +10,14 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let handle = app.app_handle();
-            let _window = tauri::WindowBuilder::new(
+            let main_window = tauri::WindowBuilder::new(
                 &handle,
                 "main",
                 tauri::WindowUrl::External("https://www.thehiveforums.com/admin_e.php?xbhm/user-manager/".parse().unwrap())
               )
               .initialization_script(include_str!("./preload.js"))
               .build().unwrap();
+              main_window.set_title("XBHive Manager");
             Ok(())
         })
         .run(tauri::generate_context!())
